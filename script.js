08d7c95fcaba95fb8152df7942d4724ea8fa7c01
IDDna <romanov1371@gmail.com>
2022-07-06 11:19:45 +0300

    Create main functions of calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+function addNumbers(numOne, numTwo) {
+   return numOne + numTwo;
+}
+
+function subtractNumbers(numOne, numTwo) {
+   return numOne - numTwo;
+}
+
+function multiplyNumbers(numOne, numTwo) {
+   return numOne * numTwo;
+}
+
+function divideNumbers(numOne, numTwo) {
+   return numOne / numTwo;
+}
+
+function changePosNeg(number) {
+   return -number;
+}
+
+console.log(divideNumbers(4384, 23));
