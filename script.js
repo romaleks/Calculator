90794e1063adc3097e32e00727221e8502425ef1
IDDna <romanov1371@gmail.com>
2022-07-10 21:14:31 +0300

    Declare all variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const calculatorText = document.querySelector('[data-previous-operand]');
+const calculatorStore = document.querySelector('[data-current-operand]');
