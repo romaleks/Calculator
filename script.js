fbeeccf7b95938df038244be0d4778936298bf7a
IDDna <romanov1371@gmail.com>
2022-07-10 21:27:47 +0300

    Create clear, appendNumber and updateDisplay functions

--- script.js
+++ script.js
@@ -3,5 +3,28 @@ const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const calculatorText = document.querySelector('[data-previous-operand]');
-const calculatorStore = document.querySelector('[data-current-operand]');
+const calculatorStore = document.querySelector('[data-previous-operand]');
+const calculatorText = document.querySelector('[data-current-operand]');
+
+let operator;
+let previousOperand = '';
+let currentOperand = '';
+
+function clear() {
+   calculatorText.textContent = '';
+   calculatorStore.textContent = '';
+   operator = undefined;
+}
+
+function appendNumber(number) {
+   if (currentOperand.length < 13) currentOperand += number;
+}
+
+function updateDisplay() {
+   calculatorText.textContent = currentOperand;
+}
+
+numberButtons.forEach(button => button.addEventListener('click', () => {
+   appendNumber(button.textContent);
+   updateDisplay();
+}));
