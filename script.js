c4ab9571d056cb1457b65ac35d4d1566b5420e59
IDDna <romanov1371@gmail.com>
2022-07-10 22:39:34 +0300

    Make input more comfortable

--- script.js
+++ script.js
@@ -67,9 +67,24 @@ function operate() {
    previousOperand = '';
 }
 
+function getDisplayNumber(number) {
+   const stringNumber = number.toString();
+   const integerDigits = parseFloat(stringNumber.split('.')[0]);
+   const decimalDigits = stringNumber.split('.')[1];
+   let integerDisplay;
+   if (isNaN(integerDigits)) {
+      integerDisplay = '';
+   } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+   }
+   if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+   } else return integerDisplay;
+}
+
 function updateDisplay() {
-   calculatorText.textContent = currentOperand;
-   calculatorStore.textContent = `${previousOperand} ${operator}`;
+   calculatorText.textContent = getDisplayNumber(currentOperand);
+   calculatorStore.textContent = `${getDisplayNumber(previousOperand)} ${operator}`;
 }
 
 allClearButton.onclick = () => clear();
